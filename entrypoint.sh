da45b36eb228e63fd82dbf197123c5d2355642a6
Konstantin Gribov <grossws@gmail.com>
2017-04-28 21:26:39 +0300

    Fixed bin/solr arguments passing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "solr" ] ; then
   chown -R solr:nobody /opt/solr
-  exec gosu solr:nobody /opt/solr/bin/solr -f -h 0.0.0.0 -p 8983 -s /opt/solr "${SOLR_OPTS[@]}"
+  exec gosu solr:nobody /opt/solr/bin/solr -f -h 0.0.0.0 -p 8983 -s /opt/solr ${SOLR_ARGS[@]}
 fi
 
 exec "$@"
